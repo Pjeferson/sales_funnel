cd41e1389dce2dca4a69ed845c50f08c5f7e36d6
Pjeferson <paulojeferson34@gmail.com>
2020-01-22 20:04:58 -0300

    Passing card through all sale stages

--- spec/features/detail_sale_progression_spec.rb
+++ spec/features/detail_sale_progression_spec.rb
@@ -1,18 +1,27 @@
 require "rails_helper"
 
 RSpec.feature "Detail sale progression", :js do
-  given!(:sale) { create(:sale, stage: :follow_up, product: "Sale to detail") }
+  given!(:sale) { 
+    create(:sale, stage: :contact, product: "Sale to detail")
+  }
 
   background do
     visit "/"
   end
 
   scenario "Opens the modal and closes it" do
-    follow_up_column  = page.find("#column-follow_up")
-    closed_column     = page.find("#column-closed")
+    proposal  = page.find("#column-proposal")
+    follow_up = page.find("#column-follow_up")
+    closing   = page.find("#column-closing")
+    closed    = page.find("#column-closed")
 
+    # Passing the card through all the stages
     card = page.find("#card-#{sale.id}")
-    card.drag_to(closed_column)
+    card.drag_to(proposal) 
+    card.drag_to(follow_up) 
+    card.drag_to(closing)
+    card.drag_to(closed)
+    
     
     # Trying access modal before click on a card
     expect(page).to have_no_selector('#modal-progression', visible: true)
@@ -23,6 +32,12 @@ RSpec.feature "Detail sale progression", :js do
     modal = page.find("#modal-progression")
     within modal do
       expect(page).to have_content "Sale to detail"
+      # Contact stage isn't created automatically on this test
+      expect(page).to have_content "Envio de proposta"
+      expect(page).to have_content "Follow-up"
+      expect(page).to have_content "Fechamento"
+      expect(page).to have_content "Ganho"
+      expect(page).to have_no_content "Perdido"
     end
 
     # Trying access a closed modal
